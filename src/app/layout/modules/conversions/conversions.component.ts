48d1d0a0e8072f572e0d0edd49640d02f9fe5bb8
Rodrigo Santiago <rsantiago643@gmail.com>
2024-07-14 15:44:05 -0600

    Se completa Conversiones

--- src/app/layout/modules/conversions/conversions.component.ts
+++ src/app/layout/modules/conversions/conversions.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conversions',
-  templateUrl: './conversions.component.html',
-  styleUrls: ['./conversions.component.scss']
+  template: `
+  <section>
+    <h1>Conversiones</h1>
+  </section>
+  `,
+  styles: ['h1 {text-align: center; font-family: "Nabla", system-ui; font-optical-sizing: auto; font-weight: 400;font-style: normal; font-variation-settings: "EDPT" 60,"EHLT" 12;font-size: xxx-large;}',
+    'section {height: 100vh; background-color: #004E75; justify-items: center; align-content: center;}'
+  ]
 })
 export class ConversionsComponent implements OnInit {
 
